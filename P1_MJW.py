5023ccafb232e33d4effa474bd6ad3c2dcdde87d
MelOnWheels <mel.j.white@gmail.com>
2014-12-25 21:51:34 -0500

    Working program 1

--- P1_MJW.py
+++ P1_MJW.py
@@ -1,1 +1,30 @@
-print "test program, please ignore."
+# Finding sum of multiples of two numbers up to a limit
+
+def highest_in_series(mul,maxi):
+	n = maxi-1
+	while n > maxi-(mul+1):
+		if n%mul==0:
+			return n
+		else: n=n-1
+
+def sum_terms(m1,l):
+	mn = highest_in_series(m1,l)
+	n = mn/m1
+	return (n/2.0)*(m1+mn)
+			
+l = input('Enter an upper limit: ')
+a1 = input('Enter the first multiple: ')
+b1 = input('Enter the second multiple: ')
+c1 = a1*b1
+
+a = sum_terms(a1,l)
+b = sum_terms(b1,l)
+c = sum_terms(c1,l)
+
+print a+b-c
+
+
+
+
+
+
